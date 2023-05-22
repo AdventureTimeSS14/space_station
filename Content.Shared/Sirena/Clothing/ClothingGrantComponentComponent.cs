025c71e6b22f17e96fb819b096820961c16fd1db
xtray85 <xtray85@hotmail.com>
2023-05-22 14:27:32 +0300

    Fix Components

--- Content.Shared/Sirena/Clothing/ClothingGrantComponentComponent.cs
+++ Content.Shared/Sirena/Clothing/ClothingGrantComponentComponent.cs
@@ -7,7 +7,7 @@ namespace Content.Shared.Sirena.Clothing
     {
         [DataField("component", required: true)]
         [AlwaysPushInheritance]
-        public EntityPrototype.ComponentRegistry Components { get; } = new();
+        public Robust.Shared.Prototypes.ComponentRegistry Components { get; } = new();
 
         [ViewVariables(VVAccess.ReadWrite)]
         public bool IsActive = false;
