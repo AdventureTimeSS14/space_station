c38c25b457b93d23a7d590ef07d1fdf4aa3a8788
FrisKisDr <87895070+FrisKisDr@users.noreply.github.com>
2023-05-26 16:27:08 +1000

    On branch fix Your branch is up to date with 'origin/fix'.
    Changes to be committed:
    	modified:   Content.Shared/CCVar/CCVars.cs
    	modified:   Resources/Changelog/ChangelogADT.yml
    	modified:   Resources/Prototypes/Maps/cluster.yml
    	modified:   Resources/Prototypes/Maps/moose.yml
    	modified:   Resources/Prototypes/Maps/omega.yml
    	modified:   Resources/Prototypes/Maps/saltern.yml

--- Content.Shared/CCVar/CCVars.cs
+++ Content.Shared/CCVar/CCVars.cs
@@ -1119,7 +1119,7 @@ namespace Content.Shared.CCVar
         /// Is the emergency shuttle allowed to be early launched.
         /// </summary>
         public static readonly CVarDef<bool> EmergencyEarlyLaunchAllowed =
-            CVarDef.Create("shuttle.emergency_early_launch_allowed", false, CVar.SERVERONLY);
+            CVarDef.Create("shuttle.emergency_early_launch_allowed", true, CVar.SERVERONLY);
 
         /// <summary>
         /// How long the emergency shuttle remains docked with the station, in seconds.
@@ -1162,7 +1162,7 @@ namespace Content.Shared.CCVar
         ///     Time in minutes after round start to auto-call the shuttle. Set to zero to disable.
         /// </summary>
         public static readonly CVarDef<int> EmergencyShuttleAutoCallTime =
-            CVarDef.Create("shuttle.auto_call_time", 90, CVar.SERVERONLY);
+            CVarDef.Create("shuttle.auto_call_time", 0, CVar.SERVERONLY);
 
         /// <summary>
         ///     Time in minutes after the round was extended (by recalling the shuttle) to call
