c1034e66278c953fee867c20a29f3aadf9674e5e
Schrodinger71 <132720404+Schrodinger71@users.noreply.github.com>
2023-11-21 23:28:30 +0300

    Create EvilTwinComponent.cs

---
+++ Content.Server/ADT/EvilTwin /EvilTwinComponent.cs
@@ -0,0 +1,11 @@
+﻿using Content.Shared.Mind;
+
+namespace Content.Server.ADT.EvilTwin;
+
+[RegisterComponent]
+public sealed partial class EvilTwinComponent : Component
+{
+    public EntityUid? TwinMindId;
+    public MindComponent? TwinMind;
+    public EntityUid? TwinEntity;
+}
