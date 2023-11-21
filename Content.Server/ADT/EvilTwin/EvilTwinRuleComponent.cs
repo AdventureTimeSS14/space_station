72905d91306ae20882843cab3bd262cd65e97bfb
Schrodinger71 <132720404+Schrodinger71@users.noreply.github.com>
2023-11-21 23:35:04 +0300

    Create EvilTwinRuleComponent.cs

---
+++ Content.Server/ADT/EvilTwin/EvilTwinRuleComponent.cs
@@ -0,0 +1,9 @@
+using Content.Server.ADT.EvilTwin.StationEvents;
+
+namespace Content.Server.ADT.EvilTwin;
+
+[RegisterComponent, Access(typeof(EvilTwinRule))]
+public sealed partial class EvilTwinRuleComponent : Component
+{
+
+}
