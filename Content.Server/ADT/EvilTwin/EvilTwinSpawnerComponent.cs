e34ac253d019a6119a49c86f9dad9eab353dc808
Schrodinger71 <132720404+Schrodinger71@users.noreply.github.com>
2023-11-21 23:37:05 +0300

    Create EvilTwinSpawnerComponent.cs

---
+++ Content.Server/ADT/EvilTwin/EvilTwinSpawnerComponent.cs
@@ -0,0 +1,9 @@
+﻿namespace Content.Server.ADT.EvilTwin;
+
+[RegisterComponent]
+public sealed partial class EvilTwinSpawnerComponent : Component
+{
+        [ViewVariables(VVAccess.ReadWrite)]
+        [DataField("target")]
+        public EntityUid TargetForce { get; set; } = EntityUid.Invalid;
+}
