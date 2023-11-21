c93f9e7eb05ca16bbfcfd1ce32475125f19d1c4a
Schrodinger71 <132720404+Schrodinger71@users.noreply.github.com>
2023-11-21 23:25:49 +0300

    Create EvilTwinRule.cs

---
+++ Content.Server/ADT/EvilTwin/StationEvents /EvilTwinRule.cs
@@ -0,0 +1,19 @@
+﻿using Content.Server.GameTicking.Rules.Components;
+using Content.Server.StationEvents.Events;
+using Robust.Shared.Random;
+
+namespace Content.Server.Backmen.EvilTwin.StationEvents;
+public sealed partial class EvilTwinRule : StationEventSystem<EvilTwinRuleComponent>
+{
+    protected override void Started(EntityUid uid, EvilTwinRuleComponent component, GameRuleComponent gameRule, GameRuleStartedEvent args)
+    {
+        base.Started(uid, component, gameRule, args);
+
+        if(!_evilTwinSystem.MakeTwin(out _))
+        {
+            Sawmill.Warning("Map not have latejoin spawnpoints for creating evil twin spawner");
+        }
+    }
+
+    [Dependency] private readonly EvilTwinSystem _evilTwinSystem = default!;
+}
