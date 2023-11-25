5575e63f2b24fba231fb04da0f5f062235bc6cac
Xelasto <telpis0105@gmail.com>
2023-11-25 10:19:32 +0300

    NamingSystem update

--- Content.Shared/Humanoid/NamingSystem.cs
+++ Content.Shared/Humanoid/NamingSystem.cs
@@ -32,6 +32,9 @@ namespace Content.Shared.Humanoid
                 case SpeciesNaming.FirstDashFirst:
                     return Loc.GetString("namepreset-firstdashfirst",
                         ("first1", GetFirstName(speciesProto, gender)), ("first2", GetFirstName(speciesProto, gender)));
+                case SpeciesNaming.FirstDashFirstDashFirst:
+                    return Loc.GetString("namepreset-firstdashfirstdashfirst",
+                        ("first1", GetFirstName(speciesProto, gender)), ("first2", GetFirstName(speciesProto, gender)), ("first3", GetFirstName(speciesProto, gender)));
                 case SpeciesNaming.FirstLast:
                 default:
                     return Loc.GetString("namepreset-firstlast",
