ebcd279afd44a702a5b071eadcd1c412a4d23a46
Schrodinger71 <sanya.podshibyakin@mail.ru>
2023-12-07 16:38:41 +0300

    new

---
+++ Content.Server/ADT/SayPosting/SayPostingSystem.cs
@@ -0,0 +1,52 @@
+using Content.Shared.ADT.SayPosting;
+using Content.Server.Chat.Managers;
+using Content.Shared.StatusEffect;
+using Robust.Shared.Random;
+
+namespace Content.Server.ADT.SayPosting;
+
+public sealed partial class SayPostingSystem : EntitySystem
+{
+
+
+    [Dependency] private readonly IRobustRandom _random = default!;
+    [Dependency] private readonly IChatManager _chatManager = default!;
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<SayPostingComponent, ComponentStartup>(SetupPosting);
+    }
+
+    private void SetupPosting(EntityUid uid, SayPostingComponent component, ComponentStartup args)
+    {
+        component.NextOfTime = _random.NextFloat(component.TimeDelaySayPosting.X, component.TimeDelaySayPosting.Y);
+    }
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        var query = EntityQueryEnumerator<SayPostingComponent>();
+        while (query.MoveNext(out var uid, out var sayposting))
+        {
+            sayposting.NextOfTime -= frameTime;
+
+            if (sayposting.NextOfTime >= 0)
+                continue;
+
+            // Set the new time.
+            sayposting.NextOfTime +=
+                _random.NextFloat(sayposting.TimeDelaySayPosting.X, sayposting.TimeDelaySayPosting.Y);
+
+            var duration = _random.NextFloat(sayposting.DurationSayPosting.X, sayposting.DurationSayPosting.Y);
+
+            // Make sure the messager time doesn't cut into the time to next incident.
+            sayposting.NextOfTime += duration;
+
+            _chatManager.ChatMessageToAll(Shared.Chat.ChatChannel.IC, "Тест");
+
+
+        }
+    }
+
+}
--- Content.Shared/ADT/SayPosting/SayPostingComponent.cs
+++ Content.Shared/ADT/SayPosting/SayPostingComponent.cs
@@ -1,13 +1,20 @@
-
+using System.Numerics;
 
 namespace Content.Shared.ADT
 {
 
-    [RegisterComponent]
-
-    public class SayPostingComponent : Component
+    [RegisterComponent, Access(typeof(SayPostingSystem))]
+    public sealed partial class SayPostingComponent : Component
     {
-        TimeSpan Time
+        [DataField("timeDelaySayPosting", required: true)]
+
+        public Vector2 TimeDelaySayPosting { get; private set; }
+
+
+        [DataField("durationSayPosting", required: true)]
+        public Vector2 DurationSayPosting { get; private set; }
+
+        public float NextOfTime;
     }
 
 
