881b2d759bc42cc59f6f0b591f3c9b7eb40cbac8
Schrodinger71 <sanya.podshibyakin@mail.ru>
2023-12-07 16:43:40 +0300

    ddd

--- Content.Server/Chat/Managers/IChatManager.cs
+++ Content.Server/Chat/Managers/IChatManager.cs
@@ -49,6 +49,5 @@ namespace Content.Server.Chat.Managers
         /// <param name="player">The player sending a chat message.</param>
         /// <returns>False if the player has violated rate limits and should be blocked from sending further messages.</returns>
         bool HandleRateLimit(ICommonSession player);
-        void ChatMessageToAll(ChatChannel iC, string v);
     }
 }
