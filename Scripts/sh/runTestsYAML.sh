a62b83da70635612fad4fd004704a64d0da16107
AserCreator <aser.ionov15@gmail.com>
2024-01-08 16:29:03 +0300

    carrying and vulpas

---
+++ Scripts/sh/runTestsYAML.sh
@@ -0,0 +1,11 @@
+cd ../../
+
+dotnet restore
+dotnet build --configuration DebugOpt --no-restore /p:WarningsAsErrors=nullable /m
+
+mkdir Scripts/logs
+rm Scripts/logs/Content.YAMLLinter.log
+dotnet run --project Content.YAMLLinter/Content.YAMLLinter.csproj --no-build -- NUnit.ConsoleOut=0 > Scripts/logs/Content.YAMLLinter.log
+
+echo "Tests complete. Press enter to continue."
+read
