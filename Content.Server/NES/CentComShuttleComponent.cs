58fb50f68921856626cf4d3a2ffdfeb06d441d12
Schrodinger71 <sanya.podshibyakin@mail.ru>
2024-01-17 21:45:18 +0300

    Update

---
+++ Content.Server/NES/CentComShuttleComponent.cs
@@ -0,0 +1,10 @@
+// namespace Content.Server.NES;
+
+// /// <summary>
+// /// ADT Added to centcom shuttle. Used for drone control.
+// /// </summary>
+// [RegisterComponent]
+// public sealed partial class CentComShuttleComponent : Component
+// {
+
+// }
