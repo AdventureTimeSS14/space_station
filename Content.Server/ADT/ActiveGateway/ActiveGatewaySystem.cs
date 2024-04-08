42bab722bf0d4f93efffa0dfd348fffa39f7d799
Schrodinger71 <sanya.podshibyakin@mail.ru>
2024-04-08 17:50:52 +0300

    Fix2

--- Content.Server/ADT/ActiveGateway/ActiveGatewaySystem.cs
+++ Content.Server/ADT/ActiveGateway/ActiveGatewaySystem.cs
@@ -10,11 +10,22 @@ public sealed class ActiveGatewaySystem : EntitySystem
     public override void Initialize()
     {
         base.Initialize();
-        SubscribeLocalEvent<ActiveGatewayComponent, ComponentStartup>(OnComponentStartup);
+        SubscribeLocalEvent<ActiveGatewayComponent, MapInitEvent>(OnComponentStartup);
     }
 
-    private void OnComponentStartup(EntityUid uid, ActiveGatewayComponent component, ComponentStartup args)
+    private void OnComponentStartup(EntityUid uid, ActiveGatewayComponent component, MapInitEvent args)
     {
+        if(!TryComp<GatewayComponent>(uid, out var gatewayComponent))
+            return;
+
+        OnStartGateway(uid, component.Enabled, component);
+    }
+
+    private void OnStartGateway(EntityUid uid, bool value, ActiveGatewayComponent? component = null)
+    {
+        if (!Resolve(uid, ref component) || component.Enabled == value)
+            return;
+
         _gatewaySystem.SetEnabled(uid, component.Enabled);
     }
 }
