553caaec35831faef9032c929cf3e1b5ea5dca8f
Schrödinger <132720404+Schrodinger71@users.noreply.github.com>
2024-04-08 19:35:26 +0300

    Update ActiveGatewaySystem.cs

--- Content.Server/ADT/ActiveGateway/ActiveGatewaySystem.cs
+++ Content.Server/ADT/ActiveGateway/ActiveGatewaySystem.cs
@@ -1,12 +1,9 @@
 using Content.Server.Gateway.Components;
 using Content.Server.Gateway.Systems;
-
 namespace Content.Server.ADT.ActiveGateway;
-
 public sealed class ActiveGatewaySystem : EntitySystem
 {
     [Dependency] private readonly GatewaySystem _gatewaySystem = default!;
-
     public override void Initialize()
     {
         base.Initialize();
@@ -15,32 +12,14 @@ public sealed class ActiveGatewaySystem : EntitySystem
 
     private void OnComponentStartup(EntityUid uid, ActiveGatewayComponent component, ComponentStartup args)
     {
-        if (!TryComp<GatewayComponent>(uid, out var gatewayComponent))
+        if(!TryComp<GatewayComponent>(uid, out var gatewayComponent))
             return;
-        try
-        {
-            _gatewaySystem.SetEnabled(uid, component.Enabled);
-        }
-        catch (Exception e)
-        {
-            return;
-        }
+        OnStartGateway(uid, component.Enabled, component);
     }
-
-    //private void OnComponentStartup(EntityUid uid, ActiveGatewayComponent component, ComponentStartup args)
-    //{
-    //    //if(!TryComp<GatewayComponent>(uid, out var gatewayComponent))
-    //    //    return;
-
-    //    OnStartGateway(uid, component.Enabled, component);
-    //    Dirty(uid, component);
-    //}
-
-    //private void OnStartGateway(EntityUid uid, bool value, ActiveGatewayComponent? component = null)
-    //{
-    //    if (!Resolve(uid, ref component) || component.Enabled == value)
-    //        return;
-
-    //    _gatewaySystem.SetEnabled(uid, component.Enabled);
-    //}
+    private void OnStartGateway(EntityUid uid, bool value, ActiveGatewayComponent? component = null)
+    {
+        if (!Resolve(uid, ref component) || component.Enabled == value)
+            return;
+        _gatewaySystem.SetEnabled(uid, component.Enabled);
+    }
 }
