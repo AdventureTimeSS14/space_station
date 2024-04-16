807452f910cf80622d18308e812dccec7c04d379
Schrodinger71 <sanya.podshibyakin@mail.ru>
2024-04-16 21:55:13 +0300

    Фиксим электроэнергию?

--- Content.Server/Power/Generation/Teg/TegNodeGroup.cs
+++ Content.Server/Power/Generation/Teg/TegNodeGroup.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq;
+﻿﻿using System.Linq;
 using Content.Server.NodeContainer;
 using Content.Server.NodeContainer.NodeGroups;
 using Content.Server.NodeContainer.Nodes;
@@ -66,10 +66,15 @@ public sealed class TegNodeGroup : BaseNodeGroup
 
     public override void LoadNodes(List<Node> groupNodes)
     {
-        DebugTools.Assert(groupNodes.Count <= 3, "The TEG has at most 3 parts");
         DebugTools.Assert(_entityManager != null);
 
         base.LoadNodes(groupNodes);
+
+        if (groupNodes.Count > 3)
+        {
+            // Somehow got more TEG parts. Probably shenanigans. Bail.
+            return;
+        }
 
         Generator = groupNodes.OfType<TegNodeGenerator>().SingleOrDefault();
         if (Generator != null)
