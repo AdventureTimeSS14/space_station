83944f08c11c125100d190260c67cbe65a0cf5e1
Schrodinger71 <sanya.podshibyakin@mail.ru>
2024-05-20 20:31:51 +0300

    Revert "Revert "обновление субмодуля""
    This reverts commit 712d3b621f7be87a1b14958b3e0f23567fa4d32f.

--- Content.Client/SSDIndicator/SSDIndicatorSystem.cs
+++ Content.Client/SSDIndicator/SSDIndicatorSystem.cs
@@ -1,4 +1,4 @@
-﻿using Content.Shared.CCVar;
+using Content.Shared.CCVar;
 using Content.Shared.Mind.Components;
 using Content.Shared.Mobs.Systems;
 using Content.Shared.NPC;
@@ -31,6 +31,7 @@ public sealed class SSDIndicatorSystem : EntitySystem
         if (component.IsSSD &&
             _cfg.GetCVar(CCVars.ICShowSSDIndicator) &&
             !args.InContainer &&
+            !args.HasStealthComponent &&
             !_mobState.IsDead(uid) &&
             !HasComp<ActiveNPCComponent>(uid) &&
             TryComp<MindContainerComponent>(uid, out var mindContainer) &&
